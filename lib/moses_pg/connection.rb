d642f0a70fdda1b39d82ddee54e58a6383cd038c
Jim Cain <jcain@interfacemedia.us>
2012-03-14 00:52:54 -0400

    Filled out the Connection functionality with a state machine

--- lib/moses_pg/connection.rb
+++ lib/moses_pg/connection.rb
@@ -1,9 +1,11 @@
 # encoding: utf-8
 
-require 'eventmachine'
 require 'etc'
+require 'eventmachine'
 require 'moses_pg/message'
 require 'moses_pg/message/buffer'
+require 'moses_pg/result'
+require 'state_machine'
 
 module MosesPG
 
@@ -11,44 +13,333 @@ module MosesPG
     Connection.connect(opts)
   end
 
+  class NullLogger
+    def trace(*s) end
+    def debug(*s) end
+    def info(*s) end
+    def warn(*s) end
+    def error(*s) end
+    def fatal(*s) end
+  end
+
   class Connection < ::EM::Connection
 
     Default_Host = 'localhost'
     Default_Port = 5432
 
+    state_machine :initial => :startup do
+      # log all transitions
+      after_transition any => any do |obj, trans|
+        obj.logger.trace { "+++ On event #{trans.event}: #{trans.from_name} => #{trans.to_name}" }
+      end
+      # entering a failure state fails the query
+      after_transition any => :connection_failed, :do => :_fail_connection
+      after_transition any => :query_failed, :do => :_fail_query
+      after_transition any => :parse_failed, :do => :_fail_parse
+      after_transition any => :bind_failed, :do => :_fail_bind
+      after_transition any => :execute_failed, :do => :_fail_execute
+
+      # entering the ready state checks the query queue and calls succeed for
+      # the previous command
+      after_transition any => :ready, :do => :_finish_previous_query
+
+      # when the bind is done, execute the portal
+      after_transition :bind_in_progress => :bind_completed, :do => :_send_execute
+
+      event :authentication_ok do
+        transition [:startup, :authorizing] => :receive_server_data
+      end
+      event :authentication_kerberos_v5 do
+        transition :startup => :unsupported_auth_method
+      end
+      event :authentication_cleartext_password do
+        transition :startup => :authorizing
+      end
+      event :authentication_md5_password do
+        transition :startup => :authorizing
+      end
+      event :authentication_scm_credential do
+        transition :startup => :unsupported_auth_method
+      end
+      event :authentication_gss do
+        transition :startup => :unsupported_auth_method
+      end
+      event :authentication_gss_continue do
+        transition :startup => :unsupported_auth_method
+      end
+      event :authentication_sspi do
+        transition :startup => :unsupported_auth_method
+      end
+
+      event :backend_key_data do
+        transition :receive_server_data => same
+      end
+      event :parameter_status do
+        transition :receive_server_data => same
+      end
+      event :notice_response do
+      end
+      event :error_response do
+        transition [:startup, :authorizing] => :connection_failed
+        transition [:query_in_progress, :query_described, :query_data_received] => :query_failed
+        transition :parse_in_progress => :parse_failed
+        transition :execute_in_progress => :execute_failed
+      end
+      event :error_reset do
+        transition :parse_failed => :ready
+        transition :bind_failed => :ready
+      end
+      event :ready_for_query do
+        transition [:receive_server_data, :query_in_progress] => :ready
+        transition :query_failed => :ready
+      end
+
+      event :query_sent do
+        transition :ready => :query_in_progress
+      end
+      event :parse_sent do
+        transition :ready => :parse_in_progress
+      end
+      event :bind_sent do
+        transition :ready => :bind_in_progress
+      end
+      event :execute_sent do
+        transition :bind_completed => :execute_in_progress
+      end
+      event :sync_sent do
+      end
+
+      event :command_complete do
+        transition [:query_in_progress, :query_described, :query_data_received] => :query_in_progress
+        transition :execute_in_progress => :ready
+      end
+      event :parse_complete do
+        transition :parse_in_progress => :ready
+      end
+      event :bind_complete do
+        transition :bind_in_progress => :bind_completed
+      end
+      event :row_description do
+        transition :query_in_progress => :query_described
+      end
+      event :data_row do
+        transition [:query_described, :query_data_received] => :query_data_received
+        transition :execute_in_progress => same
+      end
+      event :portal_suspended do
+        transition :execute_in_progress => same
+      end
+      event :empty_query_response do
+        transition :query_in_progress => same
+        transition :execute_in_progress => :ready
+      end
+
+      state :ready do
+        def execute(sql)
+          @logger.debug 'in #execute; starting immediate'
+          _send(:_send_query, [sql])
+        end
+
+        def prepare(name, sql, datatypes = nil)
+          @logger.debug 'in #prepare; starting immediate'
+          _send(:_send_parse, [name, sql, datatypes])
+        end
+
+        def execute_prepared(statement_name, *bindvars)
+          @logger.debug 'in #execute_prepared; starting immediate'
+          _send(:_send_bind, [statement_name, *bindvars])
+        end
+      end
+
+      state all - :ready do
+        def execute(sql)
+          @logger.debug 'in #execute; queueing request'
+          defer = ::EM::DefaultDeferrable.new
+          @waiting << [:_send_query, [sql], defer]
+          defer
+        end
+
+        def prepare(name, sql, datatypes = nil)
+          @logger.debug 'in #prepare; queueing request'
+          defer = ::EM::DefaultDeferrable.new
+          @waiting << [:_send_parse, [name, sql, datatypes], defer]
+          defer
+        end
+
+        def execute_prepared(statement_name, *bindvars)
+          @logger.debug 'in #execute_prepared; queueing request'
+          defer = ::EM::DefaultDeferrable.new
+          @waiting << [:_send_bind, [statement_name, *bindvars], defer]
+          defer
+        end
+      end
+
+    end
+
+    attr_reader :message, :server_params, :batch_size, :logger
+    attr_accessor :in_progress
+
     def self.connect(opts = {})
       host = opts[:host]
       port = opts[:port] || Default_Port
+      defer = ::EM::DefaultDeferrable.new
       if host
-        ::EM.connect(host, port, self, opts)
+        [defer, ::EM.connect(host, port, self, defer, opts)]
       else
-        ::EM.connect_unix_domain("/tmp/.s.PGSQL.#{port}", self, opts)
+        [defer, ::EM.connect_unix_domain("/tmp/.s.PGSQL.#{port}", self, defer, opts)]
       end
     end
 
-    def initialize(opts = {})
-      super
+    def initialize(defer, opts = {})
+      super()
       @dbname = opts[:dbname]
       @user = opts[:user] || Etc.getlogin
       @password = opts[:password]
       @buffer = MosesPG::Message::Buffer.new
+      @in_progress = defer
+      @logger = opts[:logger] || NullLogger.new
+      @batch_size = 0
+      @server_params = {}
+      @portals = {}
+      @waiting = []
+    end
+
+    def batch_size=(batch_size)
+      @batch_size = batch_size.to_i
     end
 
     def post_init
-      puts 'in #post_init'
-      send_data(MosesPG::Message::StartupMessage.new(@user, @dbname).dump)
+      send_message(MosesPG::Message::StartupMessage.new(@user, @dbname))
+      @result = Result.new
+    end
+
+    def send_message(message)
+      @logger.trace { "<<< #{message.inspect}" }
+      send_data(message.dump)
     end
 
     def receive_data(data)
-      puts "received data #{data.inspect}"
+      #@logger.trace { ">>> #{data.inspect}" }
       @buffer.receive(data) do |message_type_char, raw_message|
-        message = MosesPG::Message.create(message_type_char, raw_message)
-        p message
+        @message = MosesPG::Message.create(message_type_char, raw_message)
+        @logger.trace { ">>> #{@message.inspect}" }
+        send(@message.event, @message)
       end
     end
 
+    def authentication_cleartext_password(*args)
+      send_message(MosesPG::Message::PasswordMessage.new(@password))
+      super
+    end
+
+    def authentication_md5_password(*args)
+      hash1 = Digest::MD5.hexdigest(@password + @user)
+      hash2 = Digest::MD5.hexdigest(hash1 + @message.salt)
+      send_message(MosesPG::Message::PasswordMessage.new('md5' + hash2))
+      super
+    end
+
+    def parameter_status(*args)
+      @logger.debug { "in #parameter_status" }
+      @server_params[@message.name] = @message.value
+      super
+    end
+
+    def backend_key_data(*args)
+      @server_process_id = @message.process_id
+      @secret_key = @message.secret_key
+      super
+    end
+
+    def command_complete(*args)
+      @result.finish(@message.tag)
+      super
+    end
+
+    def row_description(*args)
+      @result.columns = @message.columns
+      super
+    end
+
+    def data_row(*args)
+      @result << @message.row
+      super
+    end
+
+    def _finish_previous_query
+      @logger.debug { "entering #_finish_previous_query; @in_progress = #{@in_progress.__id__}" }
+      # create a closure w/the current values, since calling #succeed MUST
+      # follow checking the queue
+      last_succeeded = if @in_progress
+        last_defer = @in_progress
+        last_result = @result
+        lambda { last_defer.succeed(last_result.nil? ? nil : last_result.result) }
+      end
+
+      # reset and process the next command in the queue
+      @in_progress = @result = nil
+      unless @waiting.empty?
+        action, args, defer = @waiting.shift
+        _send(action, args, defer)
+      end
+
+      # call succeed on the previous deferrable
+      last_succeeded.call if last_succeeded
+      @logger.debug 'leaving #_finish_previous_query'
+    end
+
+    def _send(action, args, defer = nil)
+      @logger.debug { "entering #_send(#{action.inspect}, ...)" }
+      @in_progress = defer || ::EM::DefaultDeferrable.new
+      send(action, *args)
+      @logger.debug { "leaving #_send(#{action.inspect}, ...); @in_progress = #{@in_progress.__id__}" }
+      @in_progress
+    end
+
+    def _send_query(sql)
+      send_message(MosesPG::Message::Query.new(sql))
+      @result = MosesPG::ResultGroup.new
+      query_sent
+    end
+
+    def _send_parse(name, sql, datatypes = nil)
+      send_message(MosesPG::Message::Parse.new(name, sql, datatypes))
+      send_message(MosesPG::Message::Flush.new)
+      parse_sent
+    end
+
+    def _send_bind(statement_name, *bindvars)
+      @statement_in_progress = statement_name
+      send_message(MosesPG::Message::Bind.new(statement_name, generate_portal_name(statement_name), bindvars, nil, nil))
+      send_message(MosesPG::Message::Flush.new)
+      bind_sent
+    end
+
+    def _send_execute
+      send_message(MosesPG::Message::Execute.new(@portals[@statement_in_progress.to_s], @batch_size))
+      send_message(MosesPG::Message::Flush.new)
+      @result = MosesPG::Result.new
+      execute_sent
+    end
+
+    def _fail_connection
+      @in_progress.fail(@message.errors['M'])
+    end
+    alias :_fail_parse :_fail_connection
+    alias :_fail_bind :_fail_connection
+
+    def _fail_query
+      @in_progress.fail(@message.errors['M'], @result.result)
+    end
+    alias :_fail_execute :_fail_query
+
     def unbind
-      puts 'in #unbind'
+      @logger.debug 'Connection closed'
+    end
+
+    def generate_portal_name(statement_name)
+      str = statement_name.to_s
+      @portals[str] = "__P__#{str}"
     end
 
   end
